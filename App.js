70fc3c3b6a3500c8813fc5313c7f9e7e6d29562a
Sendrayaperumal Balathandayutham <sendray.sanu@gmail.com>
2026-09-05 22:28:37 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,13 @@
+const appContainer = React.createElement("div", { id: "app-container" }, [
+  React.createElement("div", { id: "container1" }, [
+    React.createElement("h1", { id: "heading1" }, "I'am a h1 tag"),
+    React.createElement("h2", { id: "heading2" }, "I'am a h2 tag"),
+  ]),
+  React.createElement("div", { id: "container2" }, [
+    React.createElement("h1", { id: "heading1" }, "I'am a h1 tag"),
+    React.createElement("h2", { id: "heading2" }, "I'am a h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(appContainer);
