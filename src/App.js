2dca1a21d7a83979e74275263cdbbf38cf6227ba
Sendrayaperumal Balathandayutham <sendray.sanu@gmail.com>
2026-09-08 19:03:16 +0530

    Basic routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 import Header from "./components/Header";
 import CardContainer from "./components/CardContainer";
+import About from "./components/About";
+import Error from "./components/Error";
 
 const App = () => {
   return (
@@ -14,5 +17,17 @@ const App = () => {
   );
 };
 
+const routerConfig = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={routerConfig} />);
---
+++ src/components/About.js
@@ -0,0 +1,3 @@
+const About = () => <div>This is an About Us page!</div>;
+
+export default About;
